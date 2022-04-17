b2915411de19bb06654fd1f142a3f9913a3cd09a
himanshudhmn <work.himanshu24@gmail.com>
2022-04-17 15:44:41 +0530

    Upoading Project

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+let toggle = false;
+const menuIcon = document.querySelector(".menu-icon");
+const navigation = document.querySelector(".navigation");
+
+menuIcon.addEventListener("click", function () {
+  navigation.style.transition = "left 0.5s cubic-bezier(1, 0, 0, 1)";
+  if (!toggle) {
+    navigation.style.left = "8rem";
+    toggle = true;
+  } else {
+    navigation.style.left = "-32rem";
+    toggle = false;
+  }
+});
