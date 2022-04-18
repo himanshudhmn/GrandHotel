0718cb87891094b1503d8e7b28ed4fb2b80f5b76
himanshudhmn <work.himanshu24@gmail.com>
2022-04-19 00:13:40 +0530

    Smooth Scrolling added to Sidebar hyperlinks

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ menuIcon.addEventListener("click", function () {
     toggle = false;
   }
 });
+
+document.querySelectorAll("a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
